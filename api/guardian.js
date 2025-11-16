56bc7c22d7ec87399afaf28c1b7a49a166f516b2
Vikash Yadav <vikashec62@gmail.com>
2025-11-16 17:47:42 +0530

    Add file

---
+++ api/guardian.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const { query } = req.query;
+
+  const apiKey = "10882b1e-03cd-4fe4-982d-bef75fb5d269";
+
+  const response = await fetch(
+    `https://content.guardianapis.com/search?q=${query}&api-key=${apiKey}`
+  );
+
+  const data = await response.json();
+
+  res.setHeader("Access-Control-Allow-Origin", "*"); // enable CORS
+  res.status(200).json(data);
+}
