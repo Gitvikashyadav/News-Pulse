1ddadfaa0a9a6e67313b6fa8eaf013280ceb65d6
Vikash Kumar <vikashec62@gmail.com>
2025-11-16 18:03:38 +0530

    Update News_script.js

--- News_script.js
+++ News_script.js
@@ -14,6 +14,7 @@ function reload() {
 async function fetchNews(query) {
   // const res = await fetch(`${url3}${query}&api-key=${api3}`);
   const res = await fetch(`/api/guardian?query=${query}`);
+
   const data = await res.json();
   console.log(data);
   bindData(data.articles);
