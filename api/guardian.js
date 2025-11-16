c770d0cc851bd884444fdc0f4e04dc9797be13ad
Vikash Kumar <vikashec62@gmail.com>
2025-11-16 18:02:36 +0530

    Update guardian.js

--- api/guardian.js
+++ api/guardian.js
@@ -9,6 +9,8 @@ export default async function handler(req, res) {
 
   const data = await response.json();
 
-  res.setHeader("Access-Control-Allow-Origin", "*"); // enable CORS
+  // Allow your frontend to access this API
+  res.setHeader("Access-Control-Allow-Origin", "*"); 
   res.status(200).json(data);
 }
+
